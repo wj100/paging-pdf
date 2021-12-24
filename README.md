7e57f48ded11da3c194de6f1c0da508a08664d27
wb.wangjun21 <wb.wangjun21@mesg.corp.netease.com>
2021-12-24 20:47:07 +0800

    feat:兼容mac

---
+++ README.md
@@ -0,0 +1,53 @@
+# auto-size-echart  
+
+> 根据容器大小自动缩放echarts
+>
+## 本地测试
+App.vue为测试页面
+
+```bash
+git clone https://github.com/wj100/auto-size-echart.git
+
+yarn
+
+yarn dev
+```
+## 使用方法
+
+###  1. 安装
+
+
+
+```bash
+yarn add auto-size-echart
+```
+###  1. 使用
+```js
+import AutoSizeEchart from 'auto-size-echart';
+//options对象的键对应容器id
+const options={
+    chart1: {
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          },
+          yAxis: {
+            type: 'value',
+          },
+          series: [
+            {
+              data: [150, 230, 224, 218, 135, 147, 260],
+              type: 'line',
+            },
+          ],
+        },
+}
+ let autoSizeEchart = new AutoSizeEchart(options);
+ autoSizeEchart.init();
+```
+
+### 3.Attributes
+
+|     参数     |  类型   |                    描述                     |
+| :----------: | :-----: | :-----------------------------------------: |
+|     option     | object |     options对象的键对应容器id     |
---
+++ src/main.js
@@ -0,0 +1,94 @@
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+/**
+ * 分页下载pdf
+ * @param {string} filename  文件名
+ * @param {string} selector  元素选择器
+ * @param {string:'l'|'p'} direction l代表横向，p代表纵向
+ * @returns 
+ */
+/* 获取屏幕缩放比例 */
+function detectZoom() {
+    var ratio = 0,
+        screen = window.screen,
+        ua = navigator.userAgent.toLowerCase();
+
+    if (window.devicePixelRatio !== undefined) {
+        ratio = window.devicePixelRatio;
+    } else if (~ua.indexOf("msie")) {
+        if (screen.deviceXDPI && screen.logicalXDPI) {
+            ratio = screen.deviceXDPI / screen.logicalXDPI;
+        }
+    } else if (
+        window.outerWidth !== undefined &&
+        window.innerWidth !== undefined
+    ) {
+        ratio = window.outerWidth / window.innerWidth;
+    }
+
+    if (ratio) {
+        ratio = Math.round(ratio * 100);
+    }
+    return ratio / 100;
+}
+ function fenyePDF(filename, selector) {
+    const els = document.querySelectorAll(selector);
+    let pdf = new jsPDF("l", "pt", [1280, 720]); //横屏
+    let success = [];
+    for (let i = 0, len = els.length; i < len; i++) {
+        success.push(0);
+    }
+    return new Promise(function (resolve, reject) {
+        !(function runself(index) {
+            let html = els[index];
+            let contentWidth = html.clientWidth; // 获得该容器的宽高
+            let contentHeight = html.clientHeight;
+            /* 如果用户缩放了屏幕  就X缩放值 mac下 ratio=2 */
+            let ratio = detectZoom();
+            if (ratio !== 1) {
+                contentWidth = html.clientWidth * ratio;
+                contentHeight = html.clientHeight * ratio;
+            }
+            let canvas = document.createElement("canvas");
+            let scale = 1; // 解决清晰度问题，先放大 2倍
+            canvas.width = contentWidth * scale; // 将画布宽&&高放大两倍
+            canvas.height = contentHeight * scale;
+            //兼容mac缩放
+            canvas.getContext("2d").scale(scale, scale);
+            // isMac ? canvas.getContext("2d") : canvas.getContext("2d").scale(scale, scale);
+            let opts = {
+                canvas: canvas,
+                width: contentWidth,
+                height: contentHeight,
+                useCORS: true,
+            };
+            html2canvas(html, opts)
+                .then((canvas) => {
+                    let pageData = canvas.toDataURL("image/jpeg", 1.0); // 清晰度 0 - 1
+                    console.log(11110000, canvas);
+                    let imgWidth = 1280; //a4 555.28
+                    let imgHeight = (imgWidth / contentWidth) * contentHeight;
+                    // pdf.addImage(pageData, 'JPEG', 左，上，宽度，高度)设置
+                    pdf.addImage(pageData, "JPEG", 0, 0, imgWidth, imgHeight);
+                    pdf.addPage();
+                    return 1;
+                })
+                .then((item) => {
+                    success[index] = item;
+                    /* 每项都为1  则保存 */
+                    if (success.every((_) => _ === 1)) {
+                        /* 删除多余空白页 */
+                        pdf.deletePage(els.length + 1);
+                        pdf.save(filename);
+                        resolve(true);
+                    }
+                    if (index < els.length - 1) {
+                        index++;
+                        runself(index);
+                    }
+                });
+        })(0);
+    });
+}
+
+export default fenyePDF
